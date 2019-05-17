707c5fefb43a6bfc67cf96f90ae5eccba5e5856f
D.M <mitreski@mail.com>
2019-05-17 19:38:34 +0200

    Second attempt at line drawing.

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -3,9 +3,9 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	for (float t = 0.; t < 1.; t += .01) {
-		int x = x0 + (x1 - x0) * t;
-		int y = y0 + (y1 - y0) * t;
+	for (float x = x0; x <= x1; x++) {
+		float t = (x - x0) / (float)(x1 - x0);
+		int y = y0 * (1. - t) + y1 * t;
 		image.set(x, y, color);
 	}
 }
@@ -13,6 +13,8 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
 	line(13, 20, 80, 40, image, white);
+	line(20, 13, 40, 80, image, red);
+	line(80, 40, 13, 20, image, red);
 	image.flip_vertically(); // set origin at bottom-left
 	image.write_tga_file("output.tga");
 	return 0;

02b56011a5689937338696346ff0ff4b2564f892
D.M <mitreski@mail.com>
2019-05-18 17:45:33 +0200

    3D drawing with z-buffer.

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
-#include <cmath>
 #include <limits>
+#include <cstdlib>
+#include <cmath>
 #include "tgaimage.h"
 #include "geometry.h"
 #include "model.h"
@@ -9,6 +10,7 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
+Model *model = NULL;
 const int width = 800;
 const int height = 800;
 
@@ -43,23 +45,47 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
 	}
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-	if (t0.y > t1.y) std::swap(t0, t1);
-	if (t0.y > t2.y) std::swap(t0, t2);
-	if (t1.y > t2.y) std::swap(t1, t2);
-	int total_height = t2.y - t0.y;
-	for (int i = 0; i < total_height; i++) {
-		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
-		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
-		float alpha = (float)i / total_height;
-		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; 
-		Vec2i A = t0 + (t2 - t0) * alpha;
-		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
-		if (A.x > B.x) std::swap(A, B);
-		for (int j = A.x; j <= B.x; j++) {
-			image.set(j, t0.y + i, color);
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+	Vec3f s[2];
+	for (int i = 2; i--; ) {
+		s[i][0] = C[i] - A[i];
+		s[i][1] = B[i] - A[i];
+		s[i][2] = A[i] - P[i];
+	}
+	Vec3f u = cross(s[0], s[1]);
+	if (std::abs(u[2]) > 1e-2) 
+		return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+	return Vec3f(-1, 1, 1); 
+}
+
+
+void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+	Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+	Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+	Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++) {
+			bboxmin[j] = std::fmax(0.f, std::fmin(bboxmin[j], pts[i][j]));
+			bboxmax[j] = std::fmin(clamp[j], std::fmax(bboxmax[j], pts[i][j]));
 		}
 	}
+	Vec3f P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+			Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+			P.z = 0;
+			for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
+			if (zbuffer[int(P.x + P.y*width)] < P.z) {
+				zbuffer[int(P.x + P.y*width)] = P.z;
+				image.set(P.x, P.y, color);
+			}
+		}
+	}
+}
+
+Vec3f world2screen(Vec3f v) {
+	return Vec3f(int((v.x + 1.)*width / 2. + .5), int((v.y + 1.)*height / 2. + .5), v.z);
 }
 
 void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color, int ybuffer[]) {
@@ -77,42 +103,28 @@ void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color, int ybuffer[
 }
 
 int main(int argc, char** argv) {
-
-	{ 
-		TGAImage scene(width, height, TGAImage::RGB);
-
-		// scene "2d mesh"
-		line(Vec2i(20, 34), Vec2i(744, 400), scene, red);
-		line(Vec2i(120, 434), Vec2i(444, 400), scene, green);
-		line(Vec2i(330, 463), Vec2i(594, 200), scene, blue);
-
-		// screen line
-		line(Vec2i(10, 10), Vec2i(790, 10), scene, white);
-
-		scene.flip_vertically(); // set origin at bottom-left
-		scene.write_tga_file("scene.tga");
+	if (2 == argc) {
+		model = new Model(argv[1]);
+	}
+	else {
+		model = new Model("obj/african_head/african_head.obj");
 	}
 
-	{
-		TGAImage render(width, 16, TGAImage::RGB);
-		int ybuffer[width];
-		for (int i = 0; i < width; i++) {
-			ybuffer[i] = std::numeric_limits<int>::min();
-		}
-		rasterize(Vec2i(20, 34), Vec2i(744, 400), render, red, ybuffer);
-		rasterize(Vec2i(120, 434), Vec2i(444, 400), render, green, ybuffer);
-		rasterize(Vec2i(330, 463), Vec2i(594, 200), render, blue, ybuffer);
+	float *zbuffer = new float[width*height];
+	for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
 
-		// widen the image
-		for (int i = 0; i < width; i++) {
-			for (int j = 1; j < 16; j++) {
-				render.set(i, j, render.get(i, 0));
-			}
+	TGAImage image(width, height, TGAImage::RGB);
+	for (int i = 0; i < model -> nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		Vec3f pts[3];
+		for (int j = 0; j < 3; j++) {
+			pts[j] = world2screen(model -> vert(face[j]));
 		}
-
-		render.flip_vertically(); // set origin at bottom-left
-		render.write_tga_file("render.tga");
+		triangle(pts, zbuffer, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 	}
+
+	image.flip_vertically(); // set origin at bottom-left
+	image.write_tga_file("output.tga");
 
 	return 0;
 }

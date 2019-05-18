9df9f6f5532a17fe587276360fbe97ed6f60eb0a
D.M <mitreski@mail.com>
2019-05-18 13:03:22 +0200

    Drawing filled triangles.

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -44,9 +44,29 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
 	if (t0.y > t1.y) std::swap(t0, t1);
 	if (t0.y > t2.y) std::swap(t0, t2);
 	if (t1.y > t2.y) std::swap(t1, t2);
-	line(t0, t1, image, green);
-	line(t1, t2, image, green);
-	line(t2, t0, image, red);
+	int total_height = t2.y - t0.y;
+	for (int y = t0.y; y <= t1.y; y++) {
+		int segment_height = t1.y - t0.y + 1;
+		float alpha = (float)(y - t0.y) / total_height;
+		float beta = (float)(y - t0.y) / segment_height;
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = t0 + (t1 - t0) * beta;
+		if (A.x > B.x) std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++) {
+			image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+		}
+	}
+	for (int y = t1.y; y <= t2.y; y++) {
+		int segment_height = t2.y - t1.y + 1;
+		float alpha = (float)(y - t0.y) / total_height;
+		float beta = (float)(y - t1.y) / segment_height; 
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = t1 + (t2 - t1) * beta;
+		if (A.x > B.x) std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++) {
+			image.set(j, y, color); 
+		}
+	}
 }
 
 int main(int argc, char** argv) {
